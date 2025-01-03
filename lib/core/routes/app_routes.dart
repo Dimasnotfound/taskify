0067895d8672d728173f90b2edb26135f336ecbf
Dimasnotfound <dp4369344@gmail.com>
2025-01-03 14:32:34 +0700

    feature: add route file and make home page view

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:taskify/features/auth/presentation/pages/login_page.dart';
+import 'package:taskify/features/home/presentation/bloc/project_bloc.dart';
+import 'package:taskify/features/home/presentation/pages/home_page.dart';
+import '../routes/route_names.dart';
+
+class AppRoutes {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case RouteNames.login:
+        return MaterialPageRoute(
+          builder: (_) => const LoginPage(),
+        );
+      case RouteNames.home:
+        final token = settings.arguments as String;
+        return MaterialPageRoute(
+          builder: (context) => BlocProvider(
+            create: (context) => ProjectBloc(
+              RepositoryProvider.of(context),
+            ),
+            child: HomePage(token: token),
+          ),
+        );
+      default:
+        return MaterialPageRoute(
+          builder: (_) => const Scaffold(
+            body: Center(child: Text('Page not found')),
+          ),
+        );
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-
+import 'package:taskify/features/auth/data/datasources/auth_remote_datasource.dart';
+import 'package:taskify/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:taskify/features/auth/data/repositories/project_repository.dart';
+import 'package:taskify/features/auth/domain/usecases/login_usecase.dart';
+import 'package:taskify/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:taskify/features/home/presentation/bloc/project_bloc.dart';
 import 'core/network/api_client.dart';
-import 'features/auth/data/datasources/auth_remote_datasource.dart';
-import 'features/auth/data/repositories/auth_repository_impl.dart';
-import 'features/auth/domain/usecases/login_usecase.dart';
-import 'features/auth/presentation/bloc/auth_bloc.dart';
-import 'features/auth/presentation/pages/splash_page.dart';
-import 'features/auth/presentation/pages/login_page.dart';
+import 'core/routes/app_routes.dart';
+import 'core/routes/route_names.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
 
   final apiClient = ApiClient();
+
+  final projectRepository = ProjectRepository(apiClient.getClient());
   final authDatasource = AuthRemoteDatasource(apiClient.getClient());
   final authRepository = AuthRepositoryImpl(authDatasource);
   final loginUseCase = LoginUseCase(authRepository);
 
-  runApp(MyApp(loginUseCase: loginUseCase));
+  runApp(MyApp(
+    projectRepository: projectRepository,
+    loginUseCase: loginUseCase,
+  ));
 }
 
 class MyApp extends StatelessWidget {
+  final ProjectRepository projectRepository;
   final LoginUseCase loginUseCase;
 
-  const MyApp({Key? key, required this.loginUseCase}) : super(key: key);
+  const MyApp({
+    Key? key,
+    required this.projectRepository,
+    required this.loginUseCase,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => AuthBloc(loginUseCase),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        initialRoute: '/',
-        routes: {
-          '/': (_) => const SplashPage(),
-          '/login': (_) => const LoginPage(),
-        },
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<ProjectRepository>.value(value: projectRepository),
+        RepositoryProvider<LoginUseCase>.value(value: loginUseCase),
+      ],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<AuthBloc>(
+            create: (context) => AuthBloc(
+              RepositoryProvider.of<LoginUseCase>(context),
+            ),
+          ),
+          BlocProvider<ProjectBloc>(
+            create: (context) => ProjectBloc(
+              RepositoryProvider.of<ProjectRepository>(context),
+            ),
+          ),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          initialRoute: RouteNames.login,
+          onGenerateRoute: AppRoutes.generateRoute,
+        ),
       ),
     );
   }
